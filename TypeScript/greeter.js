807a7eba854406ec4db30cc35876a28c0e8229ec
Developer MD <matthewdelmonte@matthewd-macbookPro.local>
2019-04-20 18:17:01 -0700

    added TypeScript

---
+++ TypeScript/greeter.js
@@ -0,0 +1,15 @@
+// typeScript sample file
+var Student = /** @class */ (function () {
+    function Student(firstName, middleName, lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleName + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student('Matthew', '', 'Del Monte');
+document.body.innerHTML = greeter(user);
---
+++ TypeScript/greeter.ts
@@ -0,0 +1,20 @@
+// typeScript sample file
+
+class Student {
+  fullName: string;
+  constructor(public firstName: string, public middleName: string, public lastName: string) {
+    this.fullName = firstName + " " + middleName + " " + lastName;
+    }
+  }
+interface Person {
+  firstName: string;
+  lastName: string;
+  }
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student('Matthew', '', 'Del Monte');
+
+document.body.innerHTML = greeter(user);
