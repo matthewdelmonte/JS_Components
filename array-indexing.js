5208de244eb52f3f18f523ca1ec381776b36b796
Matthew Del Monte <mdel3089@gmail.com>
2020-02-05 22:45:55 -0800

    added array indexing file

---
+++ array-indexing.js
@@ -0,0 +1,10 @@
+// array indexing
+
+let array1 = [10, 9, 8, 7, 6];
+let [x, y, z, ...rest] = array1;
+document.write("x = " + x + "<br>");
+document.write("y = " + y + "<br>");
+document.write("z = " + z + "<br>");
+document.write("rest = " + rest + "<br>");
+
+console.log(array1);
