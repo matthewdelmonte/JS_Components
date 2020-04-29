35e396c664b00a858c4e38df0a7a43e8a8be5f77
matthewd <matthewdelmonte.fl@gmail.com>
2020-04-29 07:57:25 -0700

    styling

--- head-first/games/battleship.js
+++ head-first/games/battleship.js
@@ -8,7 +8,7 @@ let guesses = 0;
 
 let isSunk = false;
 
-while (isSunk == false) {
+/*while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a guess between 0-6):");
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
@@ -26,7 +26,7 @@ while (isSunk == false) {
             alert("MISS");
         }
     }
-}
+}*/
 
 let stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses) + "%";
 alert(stats);
