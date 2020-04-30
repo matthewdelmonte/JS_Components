243b0cd4b570c7d057c6651a01cae931958b7edb
matthewd <matthewdelmonte.fl@gmail.com>
2020-04-30 07:57:17 -0700

    added view and model

--- head-first/games/battleship.js
+++ head-first/games/battleship.js
@@ -1,32 +1,50 @@
-let location1 = Math.floor(Math.random() * 5);
-let location2 = location1 + 1;
-let location3 = location2 + 1;
-
-let guess;
-let hits = 0;
-let guesses = 0;
-
-let isSunk = false;
-
-/*while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! (enter a guess between 0-6):");
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number!");
-    } else {
-        guesses = guesses + 1;
-
-        if (guess == location1 || guess == location2 || guess == location3) {
-            alert("HIT!")
-            hits = hits + 1;
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sunk my battleship!");
-            }
-        } else {
-            alert("MISS");
-        }
+let view = {
+    displayMessage: function(msg) {
+        let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
     }
-}*/
+};
 
-let stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses) + "%";
-alert(stats);
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [
+        { locations: ["06", "16", "26"], hits: ["", "", ""] },
+        { locations: ["24", "34", "44"], hits: ["", "", ""] },
+        { locations: ["10", "11", "12"], hits: ["", "", ""] }
+        ],
+
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let locations = ship.locations;
+            let index = locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// test code sequence
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+// test the displayMessage method
+view.displayMessage("Tap tap, is this thing on?");
