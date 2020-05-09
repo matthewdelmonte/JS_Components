4b25c06a490b3a56c310b9985861fd2c69419658
matthewd <matthewdelmonte.fl@gmail.com>
2020-05-09 14:06:31 -0700

    minor mods

--- head-first/events/guess.js
+++ head-first/events/guess.js
@@ -9,12 +9,19 @@ function init() {
 
 function showAnswer(eventObj) {
     let image = eventObj.target;
-
     let name = image.id;
     name = name + ".jpg";
     image.src = "../Head-First-JavaScript-Programming-master/chapter9/" + name;
 
+    setTimeout(reblur, 2000, image);
+
     // image.src = "../Head-First-JavaScript-Programming-master/chapter9/zero.jpg";
     // using setAttribute to change src properties value
     // answer.setAttribute("src", "../Head-First-JavaScript-Programming-master/chapter9/zero.jpg");
+}
+
+function reblur(image) {
+    let name = image.id;
+    name = name + "blur.jpg";
+    image.src = "../Head-First-JavaScript-Programming-master/chapter9/" + name;
 }
